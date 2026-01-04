b846c38cb2fd65899059e93a2d6f7fb92fa4ba89
Reesav <guptareesav16@gmail.com>
2026-01-04 20:23:00 +0530

    batman

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+struct Matrix {
+    rows : u32,
+    cols : u32,
+    data : Vec<f32>
+}
+
+fn main() {
+    let mat: Matrix = Matrix {
+        rows: 1,
+        cols: 2,
+        data: Vec::new(),
+    };
+    
+    println!("Hello, world!");
+}
